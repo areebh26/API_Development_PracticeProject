915020f8ef04904b0bd79c728ef210eb450e802b
areeb <areebulhaq2005@gmail.com>
2026-03-10 22:22:48 +0500

    developed the fucntion which will get all the students from database

---
+++ controllers/students.controller.js
@@ -0,0 +1,17 @@
+import { students, students } from "../models/students.model.js";
+
+
+
+let getAllStudents = async (req,res) => {
+    try{
+        let students = students.find();
+        if(!students){
+            res.status(404).json("no record availble");
+        }else{
+            res.status(200).json(students);
+        }
+    }catch(error){
+        res.status(500).json("Something went wrong with the server");
+        console.log(error.message+"I came from getAllStudents function");
+    }
+};
