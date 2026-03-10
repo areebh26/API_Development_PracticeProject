9b0cb68f77f0b0f41cb3855dd6149ce6ded07379
areeb <areebulhaq2005@gmail.com>
2026-03-10 21:41:04 +0500

    developed the schema for the student collection

---
+++ models/students.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+let students = mongoose.model("students",mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    gender:{
+        type:String,
+        enum:["male","female","others"],
+        required:true
+    },
+    profile_pic:{
+        type:String
+    }
+}));
+export {students};
