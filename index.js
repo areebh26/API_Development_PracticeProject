3e4b0d2c1927667ae53f21848b23f1dc8efd5cbb
areeb <areebulhaq2005@gmail.com>
2026-03-12 02:01:54 +0500

    error handling for multer

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.use(express.json());
 app.use("/api/students" ,router);
 app.use("/uploads",express.static("uploads"));
 app.use(express.urlencoded({extended:false}));
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).send(`Multer Error: ${err.message}`);
+  } else if (err) {
+    return res.status(500).send(`Something went wrong: ${err.message}`);
+  }
+  next();
+});
 app.listen(process.env.PORT, () => {
     console.log('Server stared on port 3000!');
 });
